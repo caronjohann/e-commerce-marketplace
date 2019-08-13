3c82a4eb66a351bb363cdcdc9b46ad64918e3dea
Johnny Caron <caronjohann@gmail.com>
2019-08-13 15:28:19 -0400

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 // import { connect } from "react-redux"
-<<<<<<< HEAD
-// import Login from "./Login.jsx";
-// import Signup from "./Signup.jsx";
-// import Search from './Search.jsx'
-=======
 import Login from "./Login.jsx";
 import Signup from "./Signup.jsx";
 import Search from "./Search.jsx";
->>>>>>> fb3e919878b6ffd6d29a07f66fae5bda521e02f3
 import "./main.css";
 import Header from "./Header.jsx";
 // import Items from "./Items.jsx";
 import NewItems from "./NewItems.jsx";
-<<<<<<< HEAD
-// import Footer from "./Footer.jsx";
-class App extends Component {
-  render = () => {
-    return (
-      <div>
-        {/* <Signup /> */}
-        {/* <Login /> */}
-        {/* <Footer /> */}
-        <NewItems />
-        {/* <Items /> */}
-        {/* <Header /> */}
-        {/* <Items /> */}
-      </div>
-=======
 import Footer from "./Footer.jsx";
 let renderHomePage = () => {
   return (
@@ -62,7 +41,6 @@ class App extends Component {
           <Route exact={true} path="/login" render={renderLoginPage} />
         </div>
       </BrowserRouter>
->>>>>>> fb3e919878b6ffd6d29a07f66fae5bda521e02f3
     );
   };
 }
