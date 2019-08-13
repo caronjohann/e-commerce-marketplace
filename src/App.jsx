3114a0fed78ebda0c382aa11560f726426a94832
liqiu deng <isabelladeng15@gmail.com>
2019-08-13 15:45:16 -0400

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -9,39 +9,55 @@ import Header from "./Header.jsx";
 // import Items from "./Items.jsx";
 import NewItems from "./NewItems.jsx";
 import Footer from "./Footer.jsx";
-// let renderHomePage = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <Footer />
-//     </div>
-//   );
-// };
-// let renderSearchPage = () => {
-//   return (
-//     <div>
-//       <Search />
-//     </div>
-//   );
-// };
-// let renderLoginPage = () => {
-//   return (
-//     <div>
-//       <Login />
-//     </div>
-//   );
-// };
+let renderHomePage = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+let renderSearchPage = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+let renderLoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      allItems:[]
+    }
+  };
+  
   render = () => {
+    renderItems = () =>{
+      let response= await fetch("/send-items");
+      let body= await response.text();
+      console.log("send-items",body);
+      body =JSON.parse(body);
+      this.setState({allItems:body});
+    };
+    setInterval(renderItems, 500)      
+    
     return (
-      // <BrowserRouter>
-      //   <div>
-      //     <Route exact={true} path="/" render={renderHomePage} />
-      //     <Route exact={true} path="/search" render={renderSearchPage} />
-      //     <Route exact={true} path="/login" render={renderLoginPage} />
-      //   </div>
-      // </BrowserRouter>
-      <div><NewItems /></div>
+      <BrowserRouter>
+        <div>
+          <Route exact={true} path="/" render={renderHomePage} />
+          <Route exact={true} path="/search" render={renderSearchPage} />
+          <Route exact={true} path="/login" render={renderLoginPage} />
+        </div>
+      </BrowserRouter>
+     
     );
   };
 }
